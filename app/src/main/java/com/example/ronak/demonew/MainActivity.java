7513993ec362d3a337cef1e3cc9dc1d2b6fc73e2
VishakhaMajithiya <vishakha.smartsensesolutions@gmail.com>
2016-12-02 19:31:57 +0530

    change

--- app/src/main/java/com/example/ronak/demonew/MainActivity.java
+++ app/src/main/java/com/example/ronak/demonew/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.ronak.demonew;
 
 import android.app.Activity;
 import android.content.Context;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -45,11 +47,13 @@ public class MainActivity extends Activity implements LoginInt {
      * See https://g.co/AppIndexing/AndroidStudio for more information.
      */
     private GoogleApiClient client;
-    String tok = SharedPreferenceUtil.getString("token","null");
+//    String tok = SharedPreferenceUtil.getString("token","null");
+    String tok = "";
+    String token = SharedPreferenceUtil.getString("token","null token");
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if(tok != null) {
+        if(!SharedPreferenceUtil.getString("token","null token").equalsIgnoreCase(null)) {
             UtilClass.changeActivity(MainActivity.this, NeewsFeed.class, true);
             Log.e("Login","Complete "+SharedPreferenceUtil.getString("token","null Token"));
         }
@@ -80,7 +84,9 @@ public class MainActivity extends Activity implements LoginInt {
             // ATTENTION: This was auto-generated to implement the App Indexing API.
             // See https://g.co/AppIndexing/AndroidStudio for more information.
             client = new GoogleApiClient.Builder(this).addApi(AppIndex.API).build();
+
         }
+
     }
 
 
@@ -116,6 +122,8 @@ public class MainActivity extends Activity implements LoginInt {
                         if (logiObject != null) {
                             if (logiObject.optInt("status") == Constants.ResponseCode.LoginSuccessCode) {
                                 SharedPreferenceUtil.putValue(Constants.UserData.token, logiObject.optString("token"));
+                                //SharedPreferenceUtil.save();
+                                Log.e("save","Complete "+SharedPreferenceUtil.getString("token","null Token"));
                                 //Toast.makeText(getApplicationContext(),"NewsFeed Open",Toast.LENGTH_SHORT).show();
                                 JSONObject userObject = logiObject.optJSONObject("user");
                                 if (userObject != null) {
